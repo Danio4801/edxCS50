27c1ca589c148aff92aa6441c3db634b9a91bdf9
Daniel <117715001+Danio4801@users.noreply.github.com>
2023-09-19 13:58:12 +0200

    Week 1 C - lecture - part 1
    Notes and code written during part 1 of the CS50 lecture - week 1 C.

---
+++ Week 1 C/helloWName.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include<stdio.h>
+
+int main(void)
+{
+
+string first = get_string("What's your first name? ");
+string second = get_string("What's your last name? ");
+ printf("Hello World, Hello %s %s!\n", first, second);  //%s - placeholder for the string
+
+}q

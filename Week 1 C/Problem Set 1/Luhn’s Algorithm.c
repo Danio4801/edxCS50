a927041da4e13458aa5c60cc87ef0b5d2bdc0fc7
Daniel <117715001+Danio4801@users.noreply.github.com>
2023-09-26 22:04:18 +0200

    1 week successfully completed:)
    I am happy to announce that I have completed Luhn's Algorithm and "1 Week C". Luhn Algorithm can detect Visa, Amex and Mastercard and I've got (check50 15/15 • style50 1.00 •) highest grade after almost 5 hours of work. Looking forward to third week.
    check50
    cs50/problems/2023/x/credit
    :) credit.c exists
    Log
    checking that credit.c exists...
    :) credit.c compiles
    Log
    running clang credit.c -o credit -std=c11 -ggdb -lm -lcs50...
    :) identifies 378282246310005 as AMEX
    Log
    running ./credit...
    sending input 378282246310005...
    checking for output "AMEX\n"...
    checking for EOF...
    checking that program exited with status 0...
    :) identifies 371449635398431 as AMEX
    Log
    running ./credit...
    sending input 371449635398431...
    checking for output "AMEX\n"...
    checking for EOF...
    checking that program exited with status 0...
    :) identifies 5555555555554444 as MASTERCARD
    Log
    running ./credit...
    sending input 5555555555554444...
    checking for output "MASTERCARD\n"...
    checking for EOF...
    checking that program exited with status 0...
    :) identifies 5105105105105100 as MASTERCARD
    Log
    running ./credit...
    sending input 5105105105105100...
    checking for output "MASTERCARD\n"...
    checking for EOF...
    checking that program exited with status 0...
    :) identifies 4111111111111111 as VISA
    Log
    running ./credit...
    sending input 4111111111111111...
    checking for output "VISA\n"...
    checking for EOF...
    checking that program exited with status 0...
    :) identifies 4012888888881881 as VISA
    Log
    running ./credit...
    sending input 4012888888881881...
    checking for output "VISA\n"...
    checking for EOF...
    checking that program exited with status 0...
    :) identifies 4222222222222 as VISA
    Log
    running ./credit...
    sending input 4222222222222...
    checking for output "VISA\n"...
    checking for EOF...
    checking that program exited with status 0...
    :) identifies 1234567890 as INVALID
    Log
    running ./credit...
    sending input 1234567890...
    checking for output "INVALID\n"...
    checking for EOF...
    checking that program exited with status 0...
    :) identifies 369421438430814 as INVALID
    Log
    running ./credit...
    sending input 369421438430814...
    checking for output "INVALID\n"...
    checking for EOF...
    checking that program exited with status 0...
    :) identifies 4062901840 as INVALID
    Log
    running ./credit...
    sending input 4062901840...
    checking for output "INVALID\n"...
    checking for EOF...
    checking that program exited with status 0...
    :) identifies 5673598276138003 as INVALID
    Log
    running ./credit...
    sending input 5673598276138003...
    checking for output "INVALID\n"...
    checking for EOF...
    checking that program exited with status 0...
    :) identifies 4111111111111113 as INVALID
    Log
    running ./credit...
    sending input 4111111111111113...
    checking for output "INVALID\n"...
    checking for EOF...
    checking that program exited with status 0...
    :) identifies 4222222222223 as INVALID
    Log
    running ./credit...
    sending input 4222222222223...
    checking for output "INVALID\n"...
    checking for EOF...
    checking that program exited with status 0...

--- Week 1 C/Problem Set 1/Luhn’s Algorithm.c
+++ Week 1 C/Problem Set 1/Luhn’s Algorithm.c
@@ -1,23 +1,98 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
+
+int cardLength(long cardNumber);
+bool checkSum(long cardNumber, int lengthOfTheCardNumber);
+void cardIssuer(long cardNumber, int lengthOfTheCardNumber);
 
 int main(void)
 {
-    //prosba o podanie danych karty
-    long cardNumber = get_long("Insert card number here: "); //prosba o podanie numeru karty
-    printf("Your card number is: %li\n", cardNumber); //wypi1sanie na ekranie numeru karty
-    //sprawdzenie przynaleznosci do banku
-        //Ustalenie dlugosci znakow karty
-            //sprawdzenei czy karta ma 13 15 lub 16 znakow (jezeli nie to jest nieprawdziwa, odrzucamy)
-        //Determinacja czy karta jest prawdziwa
-            //wyodrebnienie co drugiej cyfry z karty (zaczynajac od lewej strony)
-                //pomnozenie kazdej z wartosci przez dwa i dodanie ich do siebie
-            //wyodrebnienie co drugiej cyfry z karty (zaczynajac od prawej strony)
-                //dodanie kazdej wyodrebnionej wartosci do siebie
-                //dodanie sumy kazdej wyodrebnionej wartosci do siebie do sumy co drugiej cyfry z karty (zaczynajac od lewej strony)pomnozonej przez dwa
-            //sprawdzenie
-                //sprawdzenie czy mod 10 daje nam zero, jezeli tak karta jest prawdziwa
-            //sprawdzenie do ktorego banku karta nalezy
-                //tbc
+    long cardNumber = get_long("Insert card number here: ");           // card number request
+    int lengthOfTheCardNumber = cardLength(cardNumber);                // check the number of characters on the card
+    bool checkSumStatus = checkSum(cardNumber, lengthOfTheCardNumber); // check if the card is valid
+    if (checkSumStatus == false)                                       // if check failed then output: invalid
+    {
+        printf("INVALID\n");
+    }
+    else // if check passed then - issuer determination (amex, visa or master)
+    {
+        cardIssuer(cardNumber, lengthOfTheCardNumber);
+    }
+    return 0;
 }
+int cardLength(long cardNumber)
+{
+    int lengthOfTheCardNumber = 0;
+    while (cardNumber > 0)
+    {
+        lengthOfTheCardNumber++;
+        cardNumber /= 10;
+    }
+    return lengthOfTheCardNumber;
+}
+bool checkSum(long cardNumber, int lengthOfTheCardNumber)
+{
+    int total = 0;
+    bool czyParzysta = false;
+    while (cardNumber > 0)
+    {
+        int currentlyLastDigit = cardNumber % 10; // pamietamy ze %10 daje nam ostatnia cyfre
+        if (czyParzysta)
+        {
+            currentlyLastDigit *= 2;
+            if (currentlyLastDigit > 9)
+            {
+                currentlyLastDigit = (currentlyLastDigit % 10) + 1;
+            }
+        }
+        total += currentlyLastDigit;
+        czyParzysta = !czyParzysta;
+        cardNumber /= 10; // pamietamy ze podzial /10 zmiennej pozwala usunac skrajnie prawa cyfre
+    }
+    return total % 10 == 0;
+}
+void cardIssuer(long cardNumber, int lengthOfTheCardNumber)
+{
+    int firstDigit, firstTwoDigits;
+
+    if (lengthOfTheCardNumber == 15)
+    {
+        firstTwoDigits = (int) (cardNumber / 10000000000000);
+        if (firstTwoDigits == 34 || firstTwoDigits == 37)
+        {
+            printf("AMEX\n");
+            return;
+        }
+    }
+    else if (lengthOfTheCardNumber == 16)
+    {
+        firstTwoDigits = (int) (cardNumber / 100000000000000);
+        if (firstTwoDigits >= 51 && firstTwoDigits <= 55)
+        {
+            printf("MASTERCARD\n");
+            return;
+        }
+        firstDigit = (int) (cardNumber / 1000000000000000);
+        if (firstDigit == 4)
+        {
+            printf("VISA\n");
+            return;
+        }
+    }
+    else if (lengthOfTheCardNumber == 13)
+    {
+        firstDigit = (int) (cardNumber / 1000000000000);
+        if (firstDigit == 4)
+        {
+            printf("VISA\n");
+            return;
+        }
+    }
+    printf("INVALID\n"); // If none of the conditions match, the issuer is undetermined
+}
+// MASTERCARD 16 DIGITS STARTS WITH 51, 52, 53, 54 OR 55
+// VISA 13 OR 16 DIGITS STARTS WITH 4
+// AMEX 15 DIGITS STARTS WITH 34 OR 37

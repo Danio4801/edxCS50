53c7edcef36c75b5fec26d492d0726b7cdb3252d
Daniel <117715001+Danio4801@users.noreply.github.com>
2023-10-01 17:35:55 +0200

    Week 2 Practice Problems - "no - vowels"
    Week 2 Practice Problems - "no - vowels"
    This program checks the validity of a entered password based on criteria:
    1. At least one lowercase letter (a-z)
    2. At least one uppercase letter (A-Z)
    3. At least one digit (0-9)
    4. At least one special character (ASCII range: 33-47, 58-64, 91-96, 123-126)
    
    Auto - test summary:
    Results for cs50/labs/2023/x/password generated by check50 v3.3.8
    :) password.c exists
    :) password.c compiles
    :) Password of 3PQvbQ6_GvneW!3R is accepted.
    :) Password of hqsk3wb. is rejected for lack of uppercase characters.
    :) Password of F-WH8PQP is rejected for lack of lowercase characters.
    :) Password of VnrHMtV4 is rejected for lack of symbols.
    :) Password of iWnktW*q is rejected for lack of numbers.

---
+++ Week 2 Arrays/Week 2 Practice Problems/password.c
@@ -0,0 +1,62 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+bool valid(string password);
+
+int main(int argc, string argv[])
+{
+    string password = get_string("Enter your password: ");
+    if (valid(password))
+    {
+        printf("Your password is valid!\n");
+    }
+    else
+    {
+        printf("Your password needs at least one uppercase letter, lowercase letter, number and symbol\n");
+    }
+    return 0;
+}
+
+bool valid(string password)
+{
+    bool lowerCase = false, upperCase = false, oneNumber = false, oneSymbol = false;
+    for (int i = 0, n = strlen(password); i < n; i++)
+    {
+        if ((password[i] >= 97) && (password[i] <= 122))
+        {
+            lowerCase = true;
+        }
+        if ((password[i] >= 65) && (password[i] <= 90))
+        {
+            upperCase = true;
+        }
+        if ((password[i] >= 48) && (password[i] <= 57))
+        {
+            oneNumber = true;
+        }
+
+        if (((password[i] >= 33) && (password[i] <= 47)) || ((password[i] >= 58) && (password[i] <= 64)) ||
+            ((password[i] >= 91) && (password[i] <= 96)) || ((password[i] >= 123) && (password[i] <= 126)))
+        {
+            oneSymbol = true;
+        }
+    }
+    if (lowerCase && upperCase && oneNumber && oneSymbol)
+    {
+        return true;
+    }
+    return false;
+}
+/*
+lowerCase from 97 to 122
+upperCase from 65 to 90
+numbers from 48 to 57
+
+specialChar:
+from 33 to 47 AND
+from 58  64 AND
+from 91 to 96 AND
+from 123 to 126
+*/
